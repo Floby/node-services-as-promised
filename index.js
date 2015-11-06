99b7b70731769e1444667470ae0e89f91259fe3e
Florent Jaby <florent.jaby@gmail.com>
2015-11-06 15:10:22 +0100

    module structure

---
+++ index.js
@@ -0,0 +1,2 @@
+exports.Container = require('./lib/container');
+exports.TestContainer = require('./lib/test-container');
---
+++ test/000-module.js
@@ -0,0 +1,31 @@
+var expect = require('chai').expect;
+
+describe('ServicesAsPromised', function () {
+  it('is a module', function () {
+    require('../index');
+  });
+
+  describe('.Container', function () {
+    var m = require('../index');
+    it('is a function', function () {
+      expect(m.Container).to.be.a('function');
+    })
+
+    it('is a constructor', function () {
+      var c = new m.Container();
+      expect(c).to.be.an.instanceof(m.Container);
+    })
+  })
+
+  describe('.TestContainer', function () {
+    var m = require('../index');
+    it('is a function', function () {
+      expect(m.TestContainer).to.be.a('function');
+    })
+
+    it('is a constructor', function () {
+      var c = new m.TestContainer();
+      expect(c).to.be.an.instanceof(m.TestContainer);
+    })
+  })
+})
